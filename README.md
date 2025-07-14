20ce3d0d1f804f2896d78d57454b1c59dfbf5091
bestia.dev <info@bestia.dev>
2025-07-14 16:40:14 +0000

    mesh

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ for faster building
 wasm-pack build --target web --profiling
 
 rsync -a --delete-after pkg/ web_server_folder/snake_bevy/pkg
+
+
+for missing apsa.pc
+from container host
+podman exec --user=root crustde_vscode_cnt apt install libasound2-dev
+
+podman exec --user=root crustde_vscode_cnt apt-get install libudev-dev
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub fn bevy_init() {
             // provide the ID selector string here
             canvas: Some("#snake_bevy_canvas".into()),
             resolution:
-                bevy::window::WindowResolution::new(500., 500.).with_scale_factor_override(1.0),
+                bevy::window::WindowResolution::new(1000., 1000.).with_scale_factor_override(1.0),
             // ... any other window properties ...
             ..default()
         }),
@@ -24,34 +24,69 @@ pub fn bevy_init() {
 
 
 #[derive(Component)]
-enum Direction {
-    Left,
-    Right,
-}
+struct Direction (f32,f32);
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>,    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,) {
     commands.spawn(Camera2d);
 
     commands.spawn((
         Sprite::from_image(asset_server.load("icon.png")),
         Transform::from_xyz(0., 0., 0.),
-        Direction::Right,
+        Direction(1.,1.),
     ));
+
+    let shapes = [
+        meshes.add(Circle::new(50.0)),
+        meshes.add(CircularSector::new(50.0, 1.0)),
+        meshes.add(CircularSegment::new(50.0, 1.25)),
+        meshes.add(Ellipse::new(25.0, 50.0)),
+        meshes.add(Annulus::new(25.0, 50.0)),
+        meshes.add(Capsule2d::new(25.0, 50.0)),
+        meshes.add(Rhombus::new(75.0, 100.0)),
+        meshes.add(Rectangle::new(50.0, 100.0)),
+        meshes.add(RegularPolygon::new(50.0, 6)),
+        meshes.add(Triangle2d::new(
+            Vec2::Y * 50.0,
+            Vec2::new(-50.0, -50.0),
+            Vec2::new(50.0, -50.0),
+        )),
+    ];
+    let num_shapes = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        // Distribute colors evenly across the rainbow.
+        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+
+        const X_EXTENT: f32 = 900.;
+        commands.spawn((
+            Mesh2d(shape),
+            MeshMaterial2d(materials.add(color)),
+            Transform::from_xyz(
+                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+                100.0,
+                0.0,
+            ),
+            Direction(1.,0.5),
+        ));
+    }
 }
 
 /// The sprite is animated by changing its translation depending on the time that has passed since
 /// the last frame.
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Right => transform.translation.x += 150. * time.delta_secs(),
-            Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
-        }
+    for (mut direction, mut transform) in &mut sprite_position {
+        
+            transform.translation.x += direction.0 * 150. * time.delta_secs();
+            transform.translation.y += direction.1 * 150. * time.delta_secs();
+        
 
-        if transform.translation.x > 200. {
-            *logo = Direction::Left;
-        } else if transform.translation.x < -200. {
-            *logo = Direction::Right;
+        if transform.translation.x > 500. || transform.translation.x < -500.{
+            direction.0 *= -1.; 
+        }
+        if transform.translation.y > 500. || transform.translation.y < -500.{
+            direction.1 *= -1.; 
         }
     }
 }
