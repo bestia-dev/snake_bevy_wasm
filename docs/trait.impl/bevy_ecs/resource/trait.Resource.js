14208804397f86155b86a78f9c4e1682147573fe
bestia.dev <info@bestia.dev>
2025-07-27 11:15:23 +0000

    update docs

---
+++ docs/trait.impl/bevy_ecs/resource/trait.Resource.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["snake_bevy_wasm",[["impl Resource for <a class=\"struct\" href=\"snake_bevy_wasm/state_in_game_mod/struct.InstanceHandle.html\" title=\"struct snake_bevy_wasm::state_in_game_mod::InstanceHandle\">InstanceHandle</a><div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> + 'static,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[542]}
