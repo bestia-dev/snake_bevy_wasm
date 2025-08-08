d09c24f7eb69d6863189e691e1e02c2a1e6677c3
bestia.dev <info@bestia.dev>
2025-08-08 18:23:59 +0000

    update docs

---
+++ docs/trait.impl/bevy_reflect/type_path/trait.TypePath.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["snake_bevy_wasm",[["impl TypePath for <a class=\"struct\" href=\"snake_bevy_wasm/struct.CustomMaterial.html\" title=\"struct snake_bevy_wasm::CustomMaterial\">CustomMaterial</a><div class=\"where\">where\n    <a class=\"struct\" href=\"snake_bevy_wasm/struct.CustomMaterial.html\" title=\"struct snake_bevy_wasm::CustomMaterial\">CustomMaterial</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/any/trait.Any.html\" title=\"trait core::any::Any\">Any</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[756]}
---
+++ docs/trait.impl/bevy_render/render_resource/bind_group/trait.AsBindGroup.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["snake_bevy_wasm",[["impl AsBindGroup for <a class=\"struct\" href=\"snake_bevy_wasm/struct.CustomMaterial.html\" title=\"struct snake_bevy_wasm::CustomMaterial\">CustomMaterial</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[186]}
